b954309132599aae6918bf82790a2961af35678b
Benja <benjaminokumu12@gmail.com>
2022-05-01 20:35:46 +0300

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# function that adds two numbers
+def add(x, y):
+    return x + y
+
+# function that subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# function that multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# function that divides two numbers
+def multiply(x, y):
+    return x / y
+
+print("select an operation")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. division")
